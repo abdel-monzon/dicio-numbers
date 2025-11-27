cfcc6d04bb75f56fbb76c075266fa58080e861b1
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-27 12:09:29 -0500

    Create SpanishDateTimeExtractor.kt

---
+++ numbers/src/main/java/org/dicio/numbers/lang/es/SpanishDateTimeExtractor.kt
@@ -0,0 +1,7 @@
+package org.dicio.numbers.lang.es
+
+import org.dicio.numbers.parser.DateTimeExtractor
+
+class SpanishDateTimeExtractor : DateTimeExtractor() {
+    override fun getLanguage(): String = "es"
+}
