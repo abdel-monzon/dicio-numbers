4dc4c7841d2c9abffb4612e70c763fd2a5327def
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-30 00:32:42 -0500

    Update SpanishNumberExtractor.kt

--- numbers/src/main/java/org/dicio/numbers/lang/es/SpanishNumberExtractor.kt
+++ numbers/src/main/java/org/dicio/numbers/lang/es/SpanishNumberExtractor.kt
@@ -1,6 +1,7 @@
 package org.dicio.numbers.lang.es
 
 import org.dicio.numbers.unit.NumberExtractor
+import org.dicio.numbers.unit.Number
 
 class SpanishNumberExtractor : NumberExtractor {
     override fun getLanguage() = "es"
