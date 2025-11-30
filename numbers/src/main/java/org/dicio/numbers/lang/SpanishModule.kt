874d4e3d4781a315014a5a3d6488dce6e86964fb
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-29 23:44:12 -0500

    Update SpanishModule.kt

--- numbers/src/main/java/org/dicio/numbers/lang/SpanishModule.kt
+++ numbers/src/main/java/org/dicio/numbers/lang/SpanishModule.kt
@@ -2,10 +2,10 @@ package org.dicio.numbers.lang
 
 import org.dicio.numbers.formatter.FormatterFactory
 import org.dicio.numbers.lang.es.SpanishFormatterFactory
-import org.dicio.numbers.lang.es.SpanishParser
-import org.dicio.numbers.lang.es.SpanishNumberExtractor
 import org.dicio.numbers.parser.ParserFactory
+import org.dicio.numbers.lang.es.SpanishParser
 import org.dicio.numbers.unit.UnitExtractor
+import org.dicio.numbers.lang.es.SpanishNumberExtractor
 
 object SpanishModule {
     fun register() {
