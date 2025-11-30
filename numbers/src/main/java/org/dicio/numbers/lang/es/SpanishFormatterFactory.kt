42bf5e5bf7eae1f62d78cf2f6b8f5e9821201606
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-30 01:19:12 -0500

    Update SpanishFormatterFactory.kt

--- numbers/src/main/java/org/dicio/numbers/lang/es/SpanishFormatterFactory.kt
+++ numbers/src/main/java/org/dicio/numbers/lang/es/SpanishFormatterFactory.kt
@@ -2,7 +2,7 @@ package org.dicio.numbers.lang.es
 
 import org.dicio.numbers.formatter.FormatterFactory
 
-class SpanishFormatterFactory : FormatterFactory {
+class SpanishFormatterFactory : FormatterFactory() {
     override fun getLanguage() = "es"
     override fun create() = SpanishFormatter()
 }
