c0ddcfb75345bf897ae7525749acd027688fd6bc
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-30 01:20:06 -0500

    Update SpanishNumberExtractor.kt

--- numbers/src/main/java/org/dicio/numbers/lang/es/SpanishNumberExtractor.kt
+++ numbers/src/main/java/org/dicio/numbers/lang/es/SpanishNumberExtractor.kt
@@ -1,9 +1,22 @@
 package org.dicio.numbers.lang.es
 
-import org.dicio.numbers.unit.NumberExtractor
+import org.dicio.numbers.parser.lexer.TokenStream
 import org.dicio.numbers.unit.Number
+import org.dicio.numbers.util.NumberExtractorUtils
 
-class SpanishNumberExtractor : NumberExtractor {
-    override fun getLanguage() = "es"
-    override fun extract(text: String): List<Number> = emptyList()
+class SpanishNumberExtractor internal constructor(
+    private val ts: TokenStream,
+    private val shortScale: Boolean
+) {
+    fun numberPreferOrdinal(): Number? = numberSignPoint(true)
+    fun numberPreferFraction(): Number? = numberSignPoint(true)
+    fun numberNoOrdinal(): Number? = numberSignPoint(false)
+
+    fun numberSignPoint(allowOrdinal: Boolean): Number? {
+        return NumberExtractorUtils.signBeforeNumber(ts) { numberInteger(allowOrdinal) }
+    }
+
+    fun numberInteger(allowOrdinal: Boolean): Number? {
+        return NumberExtractorUtils.numberLessThan1000(ts, allowOrdinal)
+    }
 }
